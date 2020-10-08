81f3d54bedddc29a9a367c858185f054ad17d739
Haonan Wang <72527916+hwang164@users.noreply.github.com>
2020-10-08 19:43:45 -0400

    Add files via upload

--- FE595-HW1.py
+++ FE595-HW1.py
@@ -1,22 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-x=np.linspace(-np.pi,np.pi,1024)
-s=np.sin(x)
-c=np.cos(x)
-plt.plot(x,s)
-plt.plot(x,c)
-plt.show()
+def main():
+    x=np.linspace(-np.pi,np.pi,1024)
+    s=np.sin(x)
+    c=np.cos(x)
+    plt.plot(x,s)
+    plt.plot(x,c)
+    plt.show()
 
 
-# In[ ]:
-
-
-
-
+if __name__=="__main__":
+    main()
