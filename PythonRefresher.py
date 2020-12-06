400d54c8ff85824576db47c3d4328a8d6ea65b3c
Haonan Wang <72527916+hwang164@users.noreply.github.com>
2020-12-06 03:57:30 +0000

    Update PythonRefresher.py

--- PythonRefresher.py
+++ PythonRefresher.py
@@ -5,9 +5,23 @@ def main():
     x=np.linspace(0,2*np.pi,1024)
     s=np.sin(x)
     c=np.cos(x)
-    plt.plot(x,s)
-    plt.plot(x,c)
+    plt.plot(x,s,label="sin")
+    plt.plot(x,c,label="cos")
+
+    tan_x = np.linspace(0, 2 * np.pi)
+    tan_y = np.tan(tan_x)
+    max_index = tan_y.argmax()
+    min_idex = tan_y.argmin()
+
+    tan_x[max_index] = np.nan
+    tan_x[min_idex] = np.nan
+    tan_y[max_index] = np.nan
+    tan_y[min_idex] = np.nan
+    plt.plot(tan_x, tan_y, color="red", label="tan")
+    
+    plt.ylim(-1, 1)
     plt.legend()
+    plt.axhline(y=0.0, color='black')
     plt.show()
 
 
