1a5ce93c3942cadbdc7dc9daeff0c10235dd6de8
Haonan Wang <72527916+hwang164@users.noreply.github.com>
2020-12-06 01:55:32 +0000

    Update FE595-HW1.py

--- FE595-HW1.py
+++ FE595-HW1.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    x=np.linspace(-np.pi,np.pi,1024)
+    x=np.linspace(0,2*np.pi,1024)
     s=np.sin(x)
     c=np.cos(x)
     plt.plot(x,s)
